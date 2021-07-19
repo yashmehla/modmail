e9f99e9140c65f90d1ab0560c5e70d9ec9fbba0d
yashmehla <84175999+yashmehla@users.noreply.github.com>
2021-07-19 18:18:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -129,7 +129,7 @@ class ModmailBot(commands.Bot):
         else:
             logger.info("MODMAIL")
         logger.info("v%s", __version__)
-        logger.info("Authors: kyb3r, fourjr, Taaku18")
+        logger.info("Authors: YASH MEHLA#9999")
         logger.line()
         logger.info("discord.py: v%s", discord.__version__)
         logger.line()
